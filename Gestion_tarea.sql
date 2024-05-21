2337ea2d069dac5cca814422bae5f93a05e33ae2
Sugeily_Cruz <sugeilycruz14@aragon.unam.mx>
2024-05-21 13:35:52 -0600

    BD

---
+++ Gestion_tarea.sql
@@ -0,0 +1,10 @@
+#CREATE DATABASE gestion_tareas;
+#USE gestion_tareas;
+#CREATE TABLE users (
+#  id INT AUTO_INCREMENT PRIMARY KEY,
+#  username VARCHAR(255) NOT NULL,
+#  password VARCHAR(255) NOT NULL
+#);
+#SHOW INDEX FROM `Tasks`
+#SHOW INDEX FROM `Users`
+SELECT * FROM Tasks
